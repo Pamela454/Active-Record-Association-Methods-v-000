eb1eb28960f5d0f3f88559b3b7b5930bc6ac0148
Pamela Frieze <pfrieze@gmail.com>
2018-06-11 20:43:12 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,10 +3,10 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    
+     self.songs.count
 
   def artist_count
-    
+
   end
 
   def all_artist_names
